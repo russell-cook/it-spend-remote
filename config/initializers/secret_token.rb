7c75ff2f468e20ad02778fb03b5cdb29526a6f7e
russell-cook <russ.cook@gmail.com>
2014-03-25 10:50:54 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItSpendRemote::Application.config.secret_key_base = '2a0cac7365235ada61e0d424c6fffea49eca0ef54185d7a8c97517a7e5becb7393dc4994b8b7abfc91c0be14f5bdf959e253d74c06585050b30e3103110c22d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItSpendRemote::Application.config.session_store :cookie_store, key: '_it-spend-remote_session'
